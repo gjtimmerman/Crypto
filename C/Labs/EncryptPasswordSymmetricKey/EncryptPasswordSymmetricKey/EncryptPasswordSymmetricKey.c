c452456af00a5f9eca1b5249cb7bbf73aa61236d
Gert Jan Timmerman <gertt@infosupport.com>
2022-12-07 16:20:44 +0100

    Implemented encrypt and decrypt

--- C/Labs/EncryptPasswordSymmetricKey/EncryptPasswordSymmetricKey/EncryptPasswordSymmetricKey.c
+++ C/Labs/EncryptPasswordSymmetricKey/EncryptPasswordSymmetricKey/EncryptPasswordSymmetricKey.c
@@ -29,6 +29,15 @@ int evaluateStatus(SECURITY_STATUS status)
 	case NTE_INVALID_PARAMETER:
 		fprintf(stderr, "Cryptographic function returned error code: NTE_INVALID_PARAMETER");
 		return 1;
+	case NTE_SILENT_CONTEXT:
+		fprintf(stderr, "Cryptographic function returned error code: NTE_SILENT_CONTEXT");
+		return 1;
+	case NTE_NO_MEMORY:
+		fprintf(stderr, "Cryptographic function returned error code: NTE_NO_MEMORY");
+		return 1;
+	case NTE_EXISTS:
+		fprintf(stderr, "Cryptographic function returned error code: NTE_EXISTS");
+		return 1;
 	case NTE_NOT_SUPPORTED:
 		fprintf(stderr, "Cryptographic function returned error code: NTE_NOT_SUPPORTED");
 		return 1;
@@ -79,49 +88,133 @@ void deriveKey(const char* password, char *keyBuffer, NCRYPT_PROV_HANDLE provHan
 void encrypt(const char* fileName, const char* password)
 {
 	NCRYPT_PROV_HANDLE provHandle;
-	NCRYPT_KEY_HANDLE keyHandle;
+//	NCRYPT_KEY_HANDLE keyHandle;
 	SECURITY_STATUS status;
 	status = NCryptOpenStorageProvider(&provHandle, 0, 0);
 	if (evaluateStatus(status) != 0)
 		return;
-	char* pkeyData = malloc(sizeof(NCRYPT_KEY_BLOB_HEADER) + 16 + sizeof(NCRYPT_AES_ALGORITHM));
-	deriveKey(password, pkeyData + sizeof(NCRYPT_KEY_BLOB_HEADER)+ sizeof(NCRYPT_AES_ALGORITHM), provHandle);
-	NCryptBufferDesc bufferDesc;
-	bufferDesc.cBuffers = 0;
-	bufferDesc.ulVersion = NCRYPTBUFFER_VERSION;
-	NCryptBuffer buffers[1];
-	bufferDesc.pBuffers = NULL;
-//	bufferDesc.pBuffers = buffers;
-	unsigned long long algId = CALG_AES_128;
-	buffers[0].BufferType = NCRYPTBUFFER_PKCS_ALG_ID;
-	buffers[0].pvBuffer = &algId;
-	buffers[0].cbBuffer = sizeof(unsigned long long);
-	NCRYPT_KEY_BLOB_HEADER *pkeyDataHeader = (NCRYPT_KEY_BLOB_HEADER*)pkeyData;
-	pkeyDataHeader->dwMagic = NCRYPT_CIPHER_KEY_BLOB_MAGIC;
-	pkeyDataHeader->cbSize = sizeof(NCRYPT_KEY_BLOB_HEADER);
+//	char* pkeyData = malloc(sizeof(NCRYPT_KEY_BLOB_HEADER) + 16 + sizeof(NCRYPT_AES_ALGORITHM));
+//	deriveKey(password, pkeyData + sizeof(NCRYPT_KEY_BLOB_HEADER) + sizeof(NCRYPT_AES_ALGORITHM), provHandle);
+//	NCryptBufferDesc bufferDesc;
+//	bufferDesc.cBuffers = 1;
+//	bufferDesc.ulVersion = NCRYPTBUFFER_VERSION;
+//	NCryptBuffer buffers[1];
+//	bufferDesc.pBuffers = NULL;
+////	bufferDesc.pBuffers = buffers;
+//	NCRYPT_KEY_BLOB_HEADER *pkeyDataHeader = (NCRYPT_KEY_BLOB_HEADER*)pkeyData;
+//	pkeyDataHeader->dwMagic = NCRYPT_CIPHER_KEY_BLOB_MAGIC;
+//	pkeyDataHeader->cbSize = sizeof(NCRYPT_KEY_BLOB_HEADER);
+//	pkeyDataHeader->cbKeyData = 16;
+//	pkeyDataHeader->cbAlgName = sizeof(NCRYPT_AES_ALGORITHM);
+//	wcscpy((wchar_t*)(pkeyData + sizeof(NCRYPT_KEY_BLOB_HEADER)), NCRYPT_AES_ALGORITHM);
+//	status = NCryptImportKey(provHandle, 0, NCRYPT_CIPHER_KEY_BLOB, NULL, &keyHandle,(PBYTE) pkeyDataHeader, (DWORD)(sizeof(NCRYPT_KEY_BLOB_HEADER) + 16 + sizeof(NCRYPT_AES_ALGORITHM)), 0);
+//	if (evaluateStatus(status) != 0)
+//		return;
+	char* pkeyData = malloc(sizeof(BCRYPT_KEY_DATA_BLOB_HEADER) + 16);
+	deriveKey(password, pkeyData + sizeof(BCRYPT_KEY_DATA_BLOB_HEADER), provHandle);
+	BCRYPT_ALG_HANDLE algProvider;
+	BCRYPT_KEY_HANDLE pKeyHandle;
+	BCRYPT_KEY_DATA_BLOB_HEADER* pkeyDataHeader = (BCRYPT_KEY_DATA_BLOB_HEADER*)pkeyData;
+	pkeyDataHeader->dwMagic = BCRYPT_KEY_DATA_BLOB_MAGIC;
+	pkeyDataHeader->dwVersion = BCRYPT_KEY_DATA_BLOB_VERSION1;
 	pkeyDataHeader->cbKeyData = 16;
-	pkeyDataHeader->cbAlgName = sizeof(NCRYPT_AES_ALGORITHM);
-	wcscpy_s((wchar_t*)pkeyDataHeader + sizeof(NCRYPT_KEY_BLOB_HEADER),sizeof(NCRYPT_AES_ALGORITHM), NCRYPT_AES_ALGORITHM);
-	status = NCryptImportKey(provHandle, 0, NCRYPT_CIPHER_KEY_BLOB, &bufferDesc, &keyHandle,(PBYTE) pkeyDataHeader, sizeof(NCRYPT_KEY_BLOB_HEADER) + 16 + sizeof(NCRYPT_AES_ALGORITHM), 0);
+	status = BCryptOpenAlgorithmProvider(&algProvider, BCRYPT_AES_ALGORITHM, 0, 0);
+	if (evaluateStatus(status) != 0)
+		return;
+
+	status = BCryptImportKey(algProvider, 0, BCRYPT_KEY_DATA_BLOB, &pKeyHandle,NULL,0, (PBYTE)pkeyDataHeader, sizeof(BCRYPT_KEY_DATA_BLOB_HEADER) + 16,0);
 	if (evaluateStatus(status) != 0)
 		return;
 	free(pkeyData);
-	//status = NCryptSetProperty(keyHandle, NCRYPT_ALGORITHM_PROPERTY, (PBYTE)NCRYPT_AES_ALGORITHM, (DWORD)(wcslen(NCRYPT_AES_ALGORITHM) + 1) * sizeof(WCHAR), 0);
-	//if (evaluateStatus(status) != 0)
-	//	return;
-	//DWORD keyLength = 128;
-	//status = NCryptSetProperty(keyHandle, NCRYPT_LENGTH_PROPERTY,(PBYTE) & keyLength, (DWORD)sizeof(DWORD), 0);
-	//if (evaluateStatus(status) != 0)
-	//	return;
-	status = NCryptFinalizeKey(keyHandle, 0);
+	FILE* inputFile = fopen(fileName, "r");
+	if (inputFile == NULL)
+		return 1;
+	char* outputFileName = malloc(strlen(fileName) + strlen(".encrypted") + 1);
+	strcpy(outputFileName, fileName);
+	strcat(outputFileName, ".encrypted");
+	FILE* outputFile = fopen(outputFileName, "w");
+	if (outputFile == NULL)
+		return 1;
+	char buffer[16];
+	int cb = fread(buffer, 1, 16, inputFile);
+	char encrypted[16];
+	DWORD cbResult;
+	while (cb == 16)
+	{
+		status = BCryptEncrypt(pKeyHandle, buffer, 16, NULL, NULL, 0, encrypted, 16, &cbResult, 0);
+		if (evaluateStatus(status) != 0)
+			return;
+		fwrite(encrypted, 1, 16, outputFile);
+		cb = fread(buffer, 1, 16, inputFile);
+	}
+	status = BCryptEncrypt(pKeyHandle, buffer, cb, NULL, NULL, 0, encrypted, 16, &cbResult, BCRYPT_BLOCK_PADDING);
+	if (evaluateStatus(status) != 0)
+		return;
+	fwrite(encrypted, 1, 16, outputFile);
+	fclose(inputFile);
+	fclose(outputFile);
+	status = BCryptDestroyKey(pKeyHandle);
 	if (evaluateStatus(status) != 0)
 		return;
 }
 
-void decrypt(const char* filename, const char* password)
+void decrypt(const char* fileName, const char* password)
 {
+	NCRYPT_PROV_HANDLE provHandle;
+	SECURITY_STATUS status;
+	status = NCryptOpenStorageProvider(&provHandle, 0, 0);
+	if (evaluateStatus(status) != 0)
+		return;
+	char* pkeyData = malloc(sizeof(BCRYPT_KEY_DATA_BLOB_HEADER) + 16);
+	deriveKey(password, pkeyData + sizeof(BCRYPT_KEY_DATA_BLOB_HEADER), provHandle);
+	BCRYPT_ALG_HANDLE algProvider;
+	BCRYPT_KEY_HANDLE pKeyHandle;
+	BCRYPT_KEY_DATA_BLOB_HEADER* pkeyDataHeader = (BCRYPT_KEY_DATA_BLOB_HEADER*)pkeyData;
+	pkeyDataHeader->dwMagic = BCRYPT_KEY_DATA_BLOB_MAGIC;
+	pkeyDataHeader->dwVersion = BCRYPT_KEY_DATA_BLOB_VERSION1;
+	pkeyDataHeader->cbKeyData = 16;
+	status = BCryptOpenAlgorithmProvider(&algProvider, BCRYPT_AES_ALGORITHM, 0, 0);
+	if (evaluateStatus(status) != 0)
+		return;
+	status = BCryptImportKey(algProvider, 0, BCRYPT_KEY_DATA_BLOB, &pKeyHandle, NULL, 0, (PBYTE)pkeyDataHeader, sizeof(BCRYPT_KEY_DATA_BLOB_HEADER) + 16, 0);
+	if (evaluateStatus(status) != 0)
+		return;
+	free(pkeyData);
+	FILE* inputFile = fopen(fileName, "r");
+	if (inputFile == NULL)
+		return 1;
+	char* outputFileName = malloc(strlen(fileName) + strlen(".decrypted") + 1);
+	strcpy(outputFileName, fileName);
+	strcat(outputFileName, ".decrypted");
+	FILE* outputFile = fopen(outputFileName, "w");
+	if (outputFile == NULL)
+		return 1;
+	char buffer[16];
+	int cb = fread(buffer, 1, 16, inputFile);
+	char decrypted[16];
+	DWORD cbResult;
+	while (cb == 16)
+	{
+		status = BCryptDecrypt(pKeyHandle, buffer, 16, NULL, NULL, 0, decrypted , 16, &cbResult, 0);
+		if (evaluateStatus(status) != 0)
+			return;
+		fwrite(decrypted, 1, 16, outputFile);
+		cb = fread(buffer, 1, 16, inputFile);
+	}
+//	status = BCryptDecrypt(pKeyHandle, pOutput, 16 * 2, NULL, NULL, 0, pDecrypted, 16 * 2, &cbResult, BCRYPT_BLOCK_PADDING);
+//	if (evaluateStatus(status) != 0)
+//		return;
+	fwrite(decrypted, 1, 16, outputFile);
+	fclose(inputFile);
+	fclose(outputFile);
 
+	if (evaluateStatus(status) != 0)
+		return;
+	status = BCryptDestroyKey(pKeyHandle);
+	if (evaluateStatus(status) != 0)
+		return;
 }
+
 
 int main(int argc, char **argv)
 {
